6a0a8d04283aff4fa90d492e9f15d510b7320f13
김태현 <taehyk528@gmail.com>
2022-07-06 03:06:39 +0900

    [#2] 배달 앱 프로젝트 생성

---
+++ Delivery/Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delivery
+//
+//  Created by taehy.k on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
