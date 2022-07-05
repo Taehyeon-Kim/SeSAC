36d17de46561fc572bc876cd8413d8f38b87681c
김태현 <taehyk528@gmail.com>
2022-07-06 03:05:14 +0900

    [#1] 영화 앱 프로젝트 생성

---
+++ Movie/Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by taehy.k on 2022/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
