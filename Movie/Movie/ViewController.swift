d08706e7f0445a2b192b9a3daf487c2039d256c9
김태현 <taehyk528@gmail.com>
2022-07-06 20:39:52 +0900

    [#10] Movie 앱 이미지 뷰 이미지 랜덤 변경

--- Movie/Movie/ViewController.swift
+++ Movie/Movie/ViewController.swift
@@ -9,7 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet var posterImageViews: [UIImageView]!
+    
+    private let posters: [String] = [
+        "7번방의선물", "겨울왕국2", "괴물", "국제시장",
+        "극한직업", "도둑들", "명량", "베테랑",
+        "부산행", "신과함께인과연", "신과함께죄와벌", "아바타",
+        "알라딘", "암살", "어벤져스엔드게임", "왕의남자",
+        "태극기휘날리며", "택시운전사", "해운대"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +27,13 @@ class ViewController: UIViewController {
             $0.layer.cornerRadius = $0.frame.width / 2
             $0.layer.borderWidth = 2.0
             $0.layer.borderColor = UIColor.systemGray3.cgColor
+        }
+    }
+    
+    @IBAction func playButtonDidTapped(_ sender: Any) {
+        backgroundImageView.image = UIImage(named: posters.randomElement() ?? "알라딘")
+        posterImageViews.forEach {
+            $0.image = UIImage(named: posters.randomElement() ?? "알라딘")
         }
     }
 }
