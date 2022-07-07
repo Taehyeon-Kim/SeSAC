c6dc4db10bea5af392776dc6ca2a1f95a35b308f
김태현 <taehyk528@gmail.com>
2022-07-07 20:13:20 +0900

    [#13] SignUpViewController 생성 및 연결

---
+++ Movie/Movie/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.swift
+//  Movie
+//
+//  Created by taehy.k on 2022/07/07.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
