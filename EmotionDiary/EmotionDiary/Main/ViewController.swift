3beb871fdb70a18d3c792d62156e1bd9d34ba47f
김태현 <taehyk528@gmail.com>
2022-07-11 20:57:38 +0900

    [#35] EmotionDiary 프로젝트 생성

---
+++ EmotionDiary/EmotionDiary/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionDiary
+//
+//  Created by taekki on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
