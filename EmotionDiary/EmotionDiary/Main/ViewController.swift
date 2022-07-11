b6ae81e8160ae77017ab5c8fbe450ea5ca67e0da
김태현 <taehyk528@gmail.com>
2022-07-12 01:03:46 +0900

    [#35] 감정 버튼 클릭 시 Label에 횟수 증가

--- EmotionDiary/EmotionDiary/Main/ViewController.swift
+++ EmotionDiary/EmotionDiary/Main/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let titles: [String] = [
+        "행복해", "사랑해", "좋아해",
+        "화가나", "속상해", "안도해",
+        "당황해", "짜증나", "눈물나"
+    ]
+    
+    private var emotionCount: [Int] = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ]
+    
+    // MARK: - Outlets
 
+    @IBOutlet var emotionIconButtons: [UIButton]!
+    @IBOutlet var emotionTitleLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureUI()
     }
-
-
 }
 
+// MARK: - Functions
+
+extension ViewController {
+
+    private func configureUI() {
+        for (index, emotionTitleLabel) in emotionTitleLabels.enumerated() {
+            emotionTitleLabel.text = titles[index] + "\(emotionCount[index])"
+        }
+    }
+
+    @IBAction func emotionButtonDidTapped(_ sender: UIButton) {
+        let index = sender.tag
+        self.emotionCount[index] += 1
+        emotionTitleLabels[index].text = titles[index] + "\(self.emotionCount[index])"
+    }
+}
