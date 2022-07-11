2e959b8bf3e6c9260efaeec30412428498a993a3
김태현 <taehyk528@gmail.com>
2022-07-11 19:42:24 +0900

    [#34] 배민 메인 화면 UI 수정 - 버튼: 이미지 + 타이틀에서 이미지 에셋으로 변경

---
+++ Delivery/Delivery/BaeminMainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaeminMainViewController.swift
+//  Delivery
+//
+//  Created by taekki on 2022/07/11.
+//
+
+import UIKit
+
+class BaeminMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.isHidden = true
+    }
+}
