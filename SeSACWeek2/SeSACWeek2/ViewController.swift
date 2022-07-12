d029e09a42ae4eedd7df9286ef2d51a69f1c8644
김태현 <taehyk528@gmail.com>
2022-07-12 11:21:52 +0900

    [#38] UIAlertController(alert, actionSheet) 실습

--- SeSACWeek2/SeSACWeek2/ViewController.swift
+++ SeSACWeek2/SeSACWeek2/ViewController.swift
@@ -8,14 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var purpleView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.presentAlert()
+    }
+    
+    func presentAlert() {
+//        let alert = UIAlertController(title: "타이틀", message: "여기는 메시지가 들어갑니다." , preferredStyle: .alert)
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        purpleView.layer.cornerRadius = 20
-        purpleView.clipsToBounds = false
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let webAction = UIAlertAction(title: "새 창으로 열기", style: .destructive, handler: nil)
+        let copyAction = UIAlertAction(title: "복사", style: .default, handler: nil)
+        
+        alert.addAction(copyAction)
+        alert.addAction(webAction)
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true) {
+            print("내부적인 처리/통계 등")
+            print("Screen Shot Capture")
+        }
     }
 }
 
