d83fa132f8214220b708d945ca0e8c1f33b86a1d
김태현 <taehyk528@gmail.com>
2022-07-15 10:43:59 +0900

    [#52] 화면 전환 실습

---
+++ SeSACWeek2/SeSACWeek2/Day10 Transition/TransitionFirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TransitionFirstViewController.swift
+//  SeSACWeek2
+//
+//  Created by taekki on 2022/07/15.
+//
+
+import UIKit
+
+class TransitionFirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindTransitionFirstVC(_ sender: UIStoryboardSegue) {}
+}
