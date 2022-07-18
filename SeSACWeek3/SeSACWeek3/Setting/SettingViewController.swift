6743ce9afadf8d47566a77e6c6fcd6910c9f0472
김태현 <taehyk528@gmail.com>
2022-07-18 20:08:14 +0900

    [#57] case 2. Dynamic Prototypes, 코드로 구현하기 - 열거형 적극 이용

---
+++ SeSACWeek3/SeSACWeek3/Setting/SettingViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SettingViewController.swift
+//  SeSACWeek3
+//
+//  Created by taekki on 2022/07/18.
+//
+
+import UIKit
+
+enum SettingType: CaseIterable, CustomStringConvertible {
+    case total
+    case personal
+    case etc
+
+    var contents: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .etc:
+            return ["고객센터/도움말"]
+        }
+    }
+    
+    var numberOfSections: Int {
+        return contents.count
+    }
+    
+    var description: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .etc:
+            return "기타"
+        }
+    }
+}
+
+final class SettingViewController: UITableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return SettingType.allCases.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return SettingType.allCases[section].description
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingType.allCases[section].numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell") else { return UITableViewCell() }
+        cell.textLabel?.text = SettingType.allCases[indexPath.section].contents[indexPath.row]
+        return cell
+    }
+}
