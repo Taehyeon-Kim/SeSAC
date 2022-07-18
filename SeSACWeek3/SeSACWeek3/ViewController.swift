505ad7e79fbecc2aed73ec24b186dccf2e774d05
김태현 <taehyk528@gmail.com>
2022-07-18 19:34:21 +0900

    [#57] Setting 스토리보드 생성

---
+++ SeSACWeek3/SeSACWeek3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek3
+//
+//  Created by taekki on 2022/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
