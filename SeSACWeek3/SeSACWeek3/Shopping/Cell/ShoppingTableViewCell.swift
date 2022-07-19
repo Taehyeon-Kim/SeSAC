56fdaa95a2c7ffdd6ab512e88ba170951833ae13
김태현 <taehyk528@gmail.com>
2022-07-20 01:10:02 +0900

    [#62] Shopping TableViewCell 생성

---
+++ SeSACWeek3/SeSACWeek3/Shopping/Cell/ShoppingTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingTableViewCell.swift
+//  SeSACWeek3
+//
+//  Created by taekki on 2022/07/20.
+//
+
+import UIKit
+
+final class ShoppingTableViewCell: UITableViewCell {
+    @IBOutlet weak var checkboxButton: UIButton!
+    @IBOutlet weak var shoppingListLabel: UILabel!
+    @IBOutlet weak var starButton: UIButton!
+}
