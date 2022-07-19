45bb33d292fe60cba45fc72b77a88ba43cfa89b1
김태현 <taehyk528@gmail.com>
2022-07-20 00:43:07 +0900

    [#62] Shopping TableViewController 생성

---
+++ SeSACWeek3/SeSACWeek3/Shopping/ShoppingTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingTableViewController.swift
+//  SeSACWeek3
+//
+//  Created by taekki on 2022/07/20.
+//
+
+import UIKit
+
+class ShoppingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
