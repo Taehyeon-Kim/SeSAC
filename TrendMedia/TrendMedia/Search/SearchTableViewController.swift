54e46ff9b5af04f5198258f68e08b66177f76c5f
김태현 <taehyk528@gmail.com>
2022-07-19 17:12:11 +0900

    [#61] Search Scene 생성

---
+++ TrendMedia/TrendMedia/Search/SearchTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SearchTableViewController.swift
+//  TrendMedia
+//
+//  Created by taekki on 2022/07/19.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+
+}
