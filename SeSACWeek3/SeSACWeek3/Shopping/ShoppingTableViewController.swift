28649632098a64615665601fa407b5df63f4a385
김태현 <taehyk528@gmail.com>
2022-07-20 01:16:24 +0900

    [#62] 기본 레이아웃 구현

--- SeSACWeek3/SeSACWeek3/Shopping/ShoppingTableViewController.swift
+++ SeSACWeek3/SeSACWeek3/Shopping/ShoppingTableViewController.swift
@@ -13,4 +13,33 @@ class ShoppingTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return 4
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "InputTableViewCell") as! InputTableViewCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell") as! ShoppingTableViewCell
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 60
+        } else {
+            return 45
+        }
+    }
 }
