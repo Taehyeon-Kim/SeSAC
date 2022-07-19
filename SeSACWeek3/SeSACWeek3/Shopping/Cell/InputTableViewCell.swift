53bf3112072c2e1f2fb026860164ccf9053ba5a8
김태현 <taehyk528@gmail.com>
2022-07-20 00:48:37 +0900

    [#62] InputTableViewCell 생성

---
+++ SeSACWeek3/SeSACWeek3/Shopping/Cell/InputTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InputTableViewCell.swift
+//  SeSACWeek3
+//
+//  Created by taekki on 2022/07/20.
+//
+
+import UIKit
+
+final class InputTableViewCell: UITableViewCell {
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var inputButton: UIButton!
+    
+    @IBAction func inputButtonTapped(_ sender: UIButton) {
+        
+    }
+}
