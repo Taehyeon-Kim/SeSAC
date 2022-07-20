1d3e26edf48a66c21e820c27ff119096236df90b
김태현 <taehyk528@gmail.com>
2022-07-21 08:59:44 +0900

    [#65] 컬렉션뷰 세팅

--- Library/Library/LibraryCollectionViewController.swift
+++ Library/Library/LibraryCollectionViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class LibraryCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.configureCollectionView()
+    }
+    
+    private func configureCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - (spacing * 3)
+        layout.itemSize = CGSize(width: width / 2, height: width / 2)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        self.collectionView.collectionViewLayout = layout
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LibraryCollectionViewCell", for: indexPath) as! LibraryCollectionViewCell
+        
+        cell.backgroundColor = .orange
+        cell.bookImageView.backgroundColor = .gray
+        
+        return cell
     }
 }
