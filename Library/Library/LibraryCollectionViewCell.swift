a3d28806930b10eef9e0237b2f1025c4a22668eb
김태현 <taehyk528@gmail.com>
2022-07-21 08:52:54 +0900

    [#65] 파일 생성 및 클래스 연결

---
+++ Library/Library/LibraryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LibraryCollectionViewCell.swift
+//  Library
+//
+//  Created by taekki on 2022/07/21.
+//
+
+import UIKit
+
+class LibraryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Library/Library/LibraryCollectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryCollectionViewController.swift
+//  Library
+//
+//  Created by taekki on 2022/07/21.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class LibraryCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
