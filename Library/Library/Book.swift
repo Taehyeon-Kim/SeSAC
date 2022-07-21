225bd4780bf3c6ede13317fdb5b4e460478e7ff3
김태현 <taehyk528@gmail.com>
2022-07-21 09:02:43 +0900

    [#65] Book 구조체 생성

---
+++ Library/Library/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  Library
+//
+//  Created by taekki on 2022/07/21.
+//
+
+import Foundation
+
+struct Book {
+    let bookTitie: String
+    let bookRate: String
+    let bookImageURL: String
+}
