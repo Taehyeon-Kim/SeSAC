88b606275193a0c5c05f3fd3213a0db6350f863d
김태현 <taehyk528@gmail.com>
2022-07-22 12:12:03 +0900

    [#68] 초기 화면 되돌리기

--- TrendMedia/TrendMedia/Search/Controllers/SearchTableViewController.swift
+++ TrendMedia/TrendMedia/Search/Controllers/SearchTableViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 final class SearchTableViewController: UITableViewController {
     private var movieInfo: MovieInfo = MovieInfo()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "처음으로", style: .plain, target: self, action: #selector(resetButtonClicked))
+    }
+    
+    @objc func resetButtonClicked() {
+        // 시작 화면 초기화
+        // iOS 13.0+ SceneDelegate 쓸 때 동작하는 코드
+        // 기존에 쌓여있던 화면들은 메모리에서 전부 내려가게 된다.
+        // 앱의 상태를 바꿀 때 window를 사용하는 편이다.
+        
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sceneDelegate = windowScene?.delegate as? SceneDelegate
+        
+        let sb = UIStoryboard(name: "Trend", bundle: nil)
+        guard let vc = sb.instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return }
+
+        sceneDelegate?.window?.rootViewController = vc
+        sceneDelegate?.window?.makeKeyAndVisible()
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movieInfo.movie.count
     }
