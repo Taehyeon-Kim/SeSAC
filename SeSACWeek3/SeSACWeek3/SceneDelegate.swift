06e9895a751abf7c94336b736a0fb780f33df1e7
김태현 <taehyk528@gmail.com>
2022-07-22 11:41:35 +0900

    [#68] 코드로 네비게이션 컨트롤러 embed

--- SeSACWeek3/SeSACWeek3/SceneDelegate.swift
+++ SeSACWeek3/SeSACWeek3/SceneDelegate.swift
@@ -13,14 +13,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     // 시작 화면을 연결하기 전에 해당 메서드에서 통제
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-
+        
         guard let scene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: scene)
         
-        let sb = UIStoryboard(name: "Trend", bundle: nil)
-        guard let vc = sb.instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return }
+        // 이 코드는 다른 화면에 배치해야 함.
+        // 시작할때마다 저장.
+        UserDefaults.standard.set(false, forKey: "First")
         
-        self.window?.rootViewController = vc // 어떤 뷰 컨트롤러를 보여주겠다.
+        // true이면 ViewController, false이면 SearchTableViewController
+        if UserDefaults.standard.bool(forKey: "First") {
+            let sb = UIStoryboard(name: "Trend", bundle: nil)
+            guard let vc = sb.instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return }
+            self.window?.rootViewController = vc // 어떤 뷰 컨트롤러를 보여주겠다.
+        } else {
+            let sb = UIStoryboard(name: "Search", bundle: nil)
+            guard let vc = sb.instantiateViewController(withIdentifier: "SearchTableViewController") as? SearchTableViewController else { return }
+            self.window?.rootViewController = UINavigationController(rootViewController: vc)
+        }
+
         self.window?.makeKeyAndVisible()     // Window가 뷰 컨트롤러를 화면에 비춰주는 역할
     }
 
