e0555bc2b20bb4a2d569ebbe7e2fb529bcc0d429
김태현 <taehyk528@gmail.com>
2022-07-26 11:04:56 +0900

    [#70] Swift Language 프로젝트 생성

---
+++ SwiftLanguage/SwiftLanguage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLanguage
+//
+//  Created by taekki on 2022/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
