395424f40126bf43944fa9af3c83f7573027f7e4
김태현 <taehyk528@gmail.com>
2022-07-27 18:49:17 +0900

    [#72] 환율 계산기 프로젝트 생성

---
+++ ExchangeRateCalculator/ExchangeRateCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExchangeRateCalculator
+//
+//  Created by taekki on 2022/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
