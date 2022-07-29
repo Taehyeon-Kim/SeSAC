648c29e8db1cb8f48c5289376734e1e0acf1747b
김태현 <taehyk528@gmail.com>
2022-07-29 09:18:56 +0900

    [#78] LocationViewController 파일 생성

---
+++ NetworkBasic/NetworkBasic/LocationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LocationViewController.swift
+//  NetworkBasic
+//
+//  Created by taekki on 2022/07/29.
+//
+
+import UIKit
+
+class LocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
