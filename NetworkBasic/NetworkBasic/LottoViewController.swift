a796c81caf41a76d60220c6d98bec730410eec54
김태현 <taehyk528@gmail.com>
2022-08-01 12:46:10 +0900

    [#79] 로또 번호 검색 서버 통신 실습(Alamofire, SwiftyJSON)

--- NetworkBasic/NetworkBasic/LottoViewController.swift
+++ NetworkBasic/NetworkBasic/LottoViewController.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 class LottoViewController: UIViewController {
     @IBOutlet weak var numberTextField: UITextField!
 //    @IBOutlet weak var lottoPickerView: UIPickerView!
     
+    @IBOutlet weak var firstNumber: UILabel!
+    @IBOutlet weak var secondNumber: UILabel!
+    @IBOutlet weak var thirdNumber: UILabel!
+    @IBOutlet weak var fourthNumber: UILabel!
+    @IBOutlet weak var fifthNumber: UILabel!
+    @IBOutlet weak var sixthNumber: UILabel!
+    @IBOutlet weak var bonusNumber: UILabel!
     
     var lottoPickerView = UIPickerView()
     // 코드로 뷰를 만드는 기능이 훨씬 더 많이 남아있음.
@@ -21,9 +31,45 @@ class LottoViewController: UIViewController {
         super.viewDidLoad()
         
         numberTextField.inputView = lottoPickerView
-        
         lottoPickerView.delegate = self
         lottoPickerView.dataSource = self
+        
+        requestLotto(number: 1025)
+    }
+    
+    func requestLotto(number: Int) {
+        // 알라모파이어에서는 AF: 200 ~ 299를 기본적으로 성공으로 간주한다.
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(number)"
+        AF.request(url, method: .get).validate(statusCode: 200..<400).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                let bonus = json["bnusNo"].stringValue
+                let firstNumber = json["drwtNo1"].stringValue
+                let secondNumber = json["drwtNo2"].stringValue
+                let thirdNumber = json["drwtNo3"].stringValue
+                let fourthNumber = json["drwtNo4"].stringValue
+                let fifthNumber = json["drwtNo5"].stringValue
+                let sixthNumber = json["drwtNo6"].stringValue
+                
+                let date = json["drwNoDate"].stringValue
+                print(date)
+                
+                self.numberTextField.text = date
+                self.firstNumber.text = firstNumber
+                self.secondNumber.text = secondNumber
+                self.thirdNumber.text = thirdNumber
+                self.fourthNumber.text = fourthNumber
+                self.fifthNumber.text = fifthNumber
+                self.sixthNumber.text = sixthNumber
+                self.bonusNumber.text = bonus
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
@@ -37,7 +83,7 @@ extension LottoViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        self.numberTextField.text = "\(numberList[row])회차"
+        requestLotto(number: numberList[row])
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
