2f1b70b7f49042f3f432d3981c09b1626e78ad99
김태현 <taehyk528@gmail.com>
2022-08-01 22:21:37 +0900

    [#81] Beer API 데이터 패치

--- NetworkBasic/NetworkBasic/BeerViewController.swift
+++ NetworkBasic/NetworkBasic/BeerViewController.swift
@@ -7,8 +7,28 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 final class BeerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchBeers()
+    }
+    
+    func fetchBeers() {
+        let url = "https://api.punkapi.com/v2/beers/random"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let array = json.array?.first
+                print("name", "\(array?["name"])")
+                print("description", "\(array?["description"])")
+                print("image_url", "\(array?["image_url"])")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
