c9f21fb2b1e2749b9cf791998108b7e8549a360f
김태현 <taehyk528@gmail.com>
2022-08-01 21:48:25 +0900

    [#81] BearViewController 생성

---
+++ NetworkBasic/NetworkBasic/BeerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BeerViewController.swift
+//  NetworkBasic
+//
+//  Created by taekki on 2022/08/01.
+//
+
+import UIKit
+
+final class BeerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
