d17d457692cf67c27d55aace7527d7165482d85b
김태현 <taehyk528@gmail.com>
2022-08-08 11:08:35 +0900

    [#93] 서버 로직 순차 호출

--- SeSACWeek6/SeSACWeek6/ViewController.swift
+++ SeSACWeek6/SeSACWeek6/ViewController.swift
@@ -14,7 +14,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(#function, "START")
         requestBlog(query: "Swift")
+        print(#function, "END")
     }
 
     
@@ -22,12 +25,41 @@ class ViewController: UIViewController {
     // - 검색 키워드
     // - 인증키
     func requestBlog(query: String) {
+        print(#function)
         guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
         let url = EndPoint.blog.requestURL + query
         
         let headers: HTTPHeaders = [
             "Authorization": "KakaoAK \(APIKey.kakao)"
         ]
+        
+        // Alamofire -> URLSession Framework -> 비동기 Request
+        // completion handler에서 Alamofire가 알아서 메인 스레드로 바꿔줌
+        AF.request(url, method: .get, headers: headers).validate().responseData { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                self.requestCafe(query: "고래밥")
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+
+    func requestCafe(query: String) {
+        print(#function)
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        let url = EndPoint.cafe.requestURL + query
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "KakaoAK \(APIKey.kakao)"
+        ]
+        
+        // Alamofire -> URLSession Framework -> 비동기 Request
+        // completion handler에서 Alamofire가 알아서 메인 스레드로 바꿔줌
         AF.request(url, method: .get, headers: headers).validate().responseData { response in
             switch response.result {
             case .success(let value):
@@ -39,6 +71,5 @@ class ViewController: UIViewController {
             }
         }
     }
-
 }
 
