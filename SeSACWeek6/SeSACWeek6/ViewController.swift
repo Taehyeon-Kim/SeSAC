803385eb105b0ed47fc94a92f620ad266f917e4a
김태현 <taehyk528@gmail.com>
2022-08-08 10:16:30 +0900

    [#93] 카카오 블로그 검색 API 연결

--- SeSACWeek6/SeSACWeek6/ViewController.swift
+++ SeSACWeek6/SeSACWeek6/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        requestBlog(query: "Swift")
     }
 
+    
+    // - Alamofire + SwiftyJSON
+    // - 검색 키워드
+    // - 인증키
+    func requestBlog(query: String) {
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        let url = EndPoint.blog.requestURL + query
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "KakaoAK \(APIKey.kakao)"
+        ]
+        AF.request(url, method: .get, headers: headers).validate().responseData { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
