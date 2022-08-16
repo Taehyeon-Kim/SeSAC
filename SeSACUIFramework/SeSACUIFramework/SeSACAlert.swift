00726a2b8def8c53d9b34bdf0d27eba1f12f36f6
김태현 <taehyk528@gmail.com>
2022-08-16 09:44:07 +0900

    [#100] Framework 실습

---
+++ SeSACUIFramework/SeSACUIFramework/SeSACAlert.swift
@@ -0,0 +1,30 @@
+//
+//  SeSACAlert.swift
+//  SeSACUIFramework
+//
+//  Created by taekki on 2022/08/16.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    open func testOpen() {}
+    
+    public func showSeSACAlert(title: String, message: String, buttonTitle: String, buttonAction: @escaping ((UIAlertAction) -> ())) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let confirm = UIAlertAction(title: buttonTitle, style: .default, handler: buttonAction)
+        
+        alert.addAction(cancel)
+        alert.addAction(confirm)
+        
+        present(alert, animated: true)
+    }
+    
+    internal func testInternal() {}
+    
+    fileprivate func testFilePrivate() {}
+    
+    private func testPrivate() {}
+}
---
+++ SeSACUIFramework/SeSACUIFramework/SeSACUIFramework.h
@@ -0,0 +1,18 @@
+//
+//  SeSACUIFramework.h
+//  SeSACUIFramework
+//
+//  Created by taekki on 2022/08/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SeSACUIFramework.
+FOUNDATION_EXPORT double SeSACUIFrameworkVersionNumber;
+
+//! Project version string for SeSACUIFramework.
+FOUNDATION_EXPORT const unsigned char SeSACUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SeSACUIFramework/PublicHeader.h>
+
+
