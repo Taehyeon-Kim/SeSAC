187bb475f41ff337c6e29ee89c534f7a915c748b
김태현 <taehyk528@gmail.com>
2022-08-16 09:45:28 +0900

    [#100] 7주차 실습 프로젝트 생성

---
+++ SeSACWeek7+Diary/SeSACWeek7+Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek7+Diary
+//
+//  Created by taekki on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
