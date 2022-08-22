95376e2b7df0116e314a0e8641489679926e8e45
김태현 <taehyk528@gmail.com>
2022-08-22 16:32:56 +0900

    [#109] 쇼핑 목록 화면으로 시작점 변경

--- SeSACWeek3/SeSACWeek3/SceneDelegate.swift
+++ SeSACWeek3/SeSACWeek3/SceneDelegate.swift
@@ -23,9 +23,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         // true이면 ViewController, false이면 SearchTableViewController
         if UserDefaults.standard.bool(forKey: "First") {
-            let sb = UIStoryboard(name: "Trend", bundle: nil)
-            guard let vc = sb.instantiateViewController(withIdentifier: "BucketlistTableViewController") as? BucketlistTableViewController else { return }
-            self.window?.rootViewController = vc // 어떤 뷰 컨트롤러를 보여주겠다.
+            let sb = UIStoryboard(name: "Shopping", bundle: nil)
+            guard let vc = sb.instantiateViewController(withIdentifier: "ShoppingTableViewController") as? ShoppingTableViewController else { return }
+            self.window?.rootViewController = UINavigationController(rootViewController: vc) // 어떤 뷰 컨트롤러를 보여주겠다.
         } else {
             let sb = UIStoryboard(name: "Search", bundle: nil)
             guard let vc = sb.instantiateViewController(withIdentifier: "SearchTableViewController") as? SearchTableViewController else { return }
@@ -34,35 +34,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         self.window?.makeKeyAndVisible()     // Window가 뷰 컨트롤러를 화면에 비춰주는 역할
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
