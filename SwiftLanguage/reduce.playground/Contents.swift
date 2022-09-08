85c63a0600bfc04c642b1081671108703a17a182
김태현 <taehyk528@gmail.com>
2022-09-08 13:29:44 +0900

    [#124] reduce(into:_) 활용

--- SwiftLanguage/reduce.playground/Contents.swift
+++ SwiftLanguage/reduce.playground/Contents.swift
@@ -39,3 +39,28 @@ genre2.forEach { dict2[$0, default: 0] += 1 }
 print(dict2)
 
 
+// 개선 2: reduce(into:_) 사용
+// 빈도 계산에 용이
+
+// initialResult는 초기값을 의미
+// 첫번째 인자가 inout 파라미터이기 때문에 initialResult로 전달 받은 값을 클로저 안에서 직접 변경 가능
+let genre3 = ["comedy", "romance", "comedy" , "comedy" , "action"]
+let genreCount = genre3.reduce(into: [:]) { $0[$1, default: 0] += 1 }
+
+print(genreCount)
+
+
+// 인접한 동일 항목 필터링
+
+let numArr1 = [1, 2, 2, 2, 2, 1, 3, 3, 3, 2, 2, 3, 5, 5, 5, 3, 4, 4, 4, 7, 4, 4]
+let numArr2 = [1, 2, 1, 6, 2, 2, 3, 5, 4, 5, 3, 4, 4, 7, 4, 4]
+
+let filterd = numArr1.reduce(into: [Int]()) { partialResult, currentValue in
+    // partialResult 배열의 길이가 0이거나, partialResult 배열의 마지막 원소가 현재 원소랑 같지 않다면
+    if (partialResult.isEmpty || partialResult.last != currentValue) {
+        partialResult.append(currentValue)
+    }
+}
+
+print(numArr1)
+print(filterd)
