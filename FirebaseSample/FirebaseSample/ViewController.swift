341d6be7864d1ee7fc3da65bd7f9ef16d8cfc47e
김태현 <taehyk528@gmail.com>
2022-10-05 10:35:36 +0900

    [#132] Firebase Sample 프로젝트 생성

---
+++ FirebaseSample/FirebaseSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseSample
+//
+//  Created by taekki on 2022/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
