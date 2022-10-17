796539450dd58a79ff6d9d1fe7d77d89d4a0acda
김태현 <taehyk528@gmail.com>
2022-10-18 00:39:47 +0900

    [#136] DetailTodo(세부 투두) 모델 추가

--- FirebaseSample/FirebaseSample/AppDelegate.swift
+++ FirebaseSample/FirebaseSample/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        aboutRealmMigration()
+        // aboutRealmMigration()
         UIViewController.swizzleMethod()
         FirebaseApp.configure()
         
--- FirebaseSample/FirebaseSample/Todo.swift
+++ FirebaseSample/FirebaseSample/Todo.swift
@@ -11,18 +11,34 @@ import RealmSwift
 final class Todo: Object {
     
     @Persisted var title: String
-    @Persisted var favorite: Double
-    @Persisted var userDescription: String
-    @Persisted var count: Double
+    @Persisted var importance: Int
     
     @Persisted(primaryKey: true) var objectId: ObjectId
     
     convenience init(
         title: String,
         importance: Int
+        
     ) {
         self.init()
         self.title = title
-        self.favorite = Double(importance)
+        self.importance = importance
+    }
+}
+
+final class DetailTodo: Object {
+    @Persisted var detailTitle: String
+    @Persisted var favorite: Bool
+    
+    @Persisted(primaryKey: true) var objectId: ObjectId
+    
+    convenience init(
+        detailTitle: String,
+        favorite: Bool
+        
+    ) {
+        self.init()
+        self.detailTitle = detailTitle
+        self.favorite = favorite
     }
 }
