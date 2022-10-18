fa8c98227b43b3b333e6b3e95eb7d4b8a044c9a7
김태현 <taehyk528@gmail.com>
2022-10-18 09:57:35 +0900

    [#137] TableViewController 생성

---
+++ SeSACWeek1617/SeSACWeek1617/SimpleTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SimpleTableViewController.swift
+//  SeSACWeek1617
+//
+//  Created by taekki on 2022/10/18.
+//
+
+import UIKit
+
+class SimpleTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
