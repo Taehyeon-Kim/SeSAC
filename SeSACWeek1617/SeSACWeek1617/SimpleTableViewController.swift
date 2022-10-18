ca7577a6a4eb3f462a5bbf9768352dc8d1b93e9c
김태현 <taehyk528@gmail.com>
2022-10-18 10:03:23 +0900

    [#137] TableView 기본 구조 구현

--- SeSACWeek1617/SeSACWeek1617/SimpleTableViewController.swift
+++ SeSACWeek1617/SeSACWeek1617/SimpleTableViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class SimpleTableViewController: UITableViewController {
+final class SimpleTableViewController: UITableViewController {
 
+    private let burgers = ["슈비버거", "프랭크", "자갈치", "고래밥"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return burgers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = burgers[indexPath.row]
+        return cell
+    }
 }
-
