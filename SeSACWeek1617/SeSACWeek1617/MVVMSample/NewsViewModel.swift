573087121b3473047668ba716a2a65f2d89281eb
김태현 <taehyk528@gmail.com>
2022-10-26 10:58:19 +0900

    [#142] Subject > Relay로 변경

--- SeSACWeek1617/SeSACWeek1617/MVVMSample/NewsViewModel.swift
+++ SeSACWeek1617/SeSACWeek1617/MVVMSample/NewsViewModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 import RxSwift
+import RxCocoa
 
 /// 글자(데이터)를 ViewModel에서 가지도록 구현한다.
 class NewsViewModel {
@@ -15,8 +16,9 @@ class NewsViewModel {
     // var pageNumber: CObservable<String> = CObservable("3000")
     // var news: CObservable<[News.NewsItem]> = CObservable(News.items)
     
-    var pageNumber = BehaviorSubject(value: "3000")
-    var news = BehaviorSubject(value: News.items)
+    var pageNumber = BehaviorRelay(value: "3000")
+    // var news = BehaviorSubject(value: News.items)
+    var news = BehaviorRelay(value: News.items)
     
     /// PageNumber의 Format을 변경해주는 메서드
     func changePageNumberFormat(of text: String) {
@@ -27,7 +29,7 @@ class NewsViewModel {
         let text = text.replacingOccurrences(of: ",", with: "")
         guard let number = Int(text) else { return }
         let result = numberFormatter.string(for: number)!
-        pageNumber.onNext(result)
+        pageNumber.accept(result)
     }
     
     /*
@@ -35,10 +37,10 @@ class NewsViewModel {
      */
     
     func resetNews() {
-        news.onNext([])
+        news.accept([])
     }
     
     func loadNews() {
-        news.onNext(News.items)
+        news.accept(News.items)
     }
 }
