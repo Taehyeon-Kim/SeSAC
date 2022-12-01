dfd66583a1ab7aae8860d5ca0ce6b12537c8c259
김태현 <taehyk528@gmail.com>
2022-12-01 12:05:18 +0900

    [#157] Unit 테스트 정리 마무리

--- InAppPurchase/InAppPurchaseTests/APIServiceTests.swift
+++ InAppPurchase/InAppPurchaseTests/APIServiceTests.swift
@@ -30,6 +30,17 @@ final class APIServiceTests: XCTestCase {
         }
     }
     
+    /*
+     네트워크: 아이폰 네트워크 응답x, api 서버 점검, api 지연
+     효율적/속도/같은 조건에서 테스트 해야 하는데 그 조건이 깨짐
+     테스트 대상이 외부 격리 X - 예측 불가능한 상황을 만들면 안됨
+     => 실제 네트워크 동작이 되는 것처럼 보이게 별개의 객체를 만듬
+     => 테스트 더블(Test Double) 테스트 코드랑 상호작용 할 수 있는 가짜 객체 생성(ex. 스턴트맨)
+     ex. dummy, mock, fake, stub, spy...
+     
+     F.I.R.S.T 원칙
+     */
+    
     // 비동기: expectation, fulfill, wait
     // 테스트는 비동기를 신경쓰지 않는다.
     // 핸들러가 기다리지 않는다.
