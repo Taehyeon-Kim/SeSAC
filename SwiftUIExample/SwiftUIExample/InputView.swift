f03cad471b2d4fa200d89a40e0e7b027885a586a
김태현 <taehyk528@gmail.com>
2023-01-06 11:01:07 +0900

    [#161] SwiftUI UIViewRepresentable
    - SwiftUI 기반 작업

--- SwiftUIExample/SwiftUIExample/InputView.swift
+++ SwiftUIExample/SwiftUIExample/InputView.swift
@@ -17,16 +17,42 @@ struct UserTextView: View {
   }
 }
 
+// UIView 객체(UIKit 요소)를 SwiftUI 내에서 사용할 수 있음
+struct ChatTextView: UIViewRepresentable {
+  // makeUIView의 return type을 명확하게 명시해주면
+  // updateUIView의 첫번째 인자인 UIViewType의 구체적으로 정해지기 때문에
+  // 특정 타입의 프로퍼티와 메서드를 호출해서 사용할 수 있게 된다.
+  
+  @Binding var nickname: String
+  
+  // SwiftUI 객체가 렌더링될때 계속 호출
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+    uiView.text = nickname // Binding 프로퍼티로 받아온 값을 업데이트
+  }
+  
+  func makeUIView(context: Context) -> UITextView {
+    let textView: UITextView = {
+      let textView = UITextView()
+      textView.backgroundColor = [UIColor.red, UIColor.blue, UIColor.green].randomElement()
+      return textView
+    }()
+    
+    return textView
+  }
+}
+
 struct InputView: View {
   
-  @State private var nickname = ""
+  @State private var nickname = "InputView의 State 프로퍼티"
   
   var body: some View {
     VStack {
-      TextField("닉네임을 입력해주세요", text: $nickname)
-        .textFieldStyle(.roundedBorder)
-        .padding(10)
-      UserTextView(text: $nickname)
+      // TextField("닉네임을 입력해주세요", text: $nickname, axis: .vertical)
+      //   .textFieldStyle(.roundedBorder)
+      //   .padding(10)
+      //   .lineLimit(0)
+      // UserTextView(text: $nickname)
+      ChatTextView(nickname: $nickname)
     }
   }
 }
